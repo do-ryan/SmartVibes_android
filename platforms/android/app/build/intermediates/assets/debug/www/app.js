305ec857c6dbb273d1b1ac5b832c59275fb85da1
Ryan Do <ryan.do@mail.utoronto.ca>
2018-12-01 23:41:30 -0500

    all metrics implemented, tare functionality

--- platforms/android/app/build/intermediates/assets/debug/www/app.js
+++ platforms/android/app/build/intermediates/assets/debug/www/app.js
@@ -1,17 +1,33 @@
 // JavaScript code for the TI SensorTag Demo app.
 
+// load math.js
+//import * as math from 'mathjs';
+
 /**
  * Object that holds application data and functions.
  */
+
 var app = {};
 
+app.SIGFIGS = 5;
+app.tare = {x:0, y:0, z:0}
 /**
- * Data that is plotted on the canvas.
+ * Variable data that is stored and displayed
  */
 app.dataPoints = [];
+// data in the plot
 app.allData = [];
-
+// all data since start of test
+app.allDataTime = [];
+// sampling time steps since start of test
 app.last_sampled_time = 0;
+// raw time fo last sampled datapoint
+app.arms = 0; 
+// root mean square acceleration
+app.peak = 0;
+// peak acceleration
+app.risk_level = 0;
+// overall risk level
 
 
 /**
@@ -28,7 +44,7 @@ app.sensortag = {};
 app.sensortag.MOVEMENT_SERVICE = 'f000aa80-0451-4000-b000-000000000000';
 app.sensortag.MOVEMENT_DATA = 'f000aa81-0451-4000-b000-000000000000';
 app.sensortag.MOVEMENT_CONFIG = 'f000aa82-0451-4000-b000-000000000000';
-app.sensortag.MOVEMENT_PERIOD = 'f000aa83-0451-4000-b000-000000000000';
+app.sensortag.MOVEMENT_PERIOD = 'f000aa83-0451-4000-b000-000000000000'; // can change period here
 app.sensortag.MOVEMENT_NOTIFICATION = '00002902-0000-1000-8000-00805f9b34fb';
 
 /**
@@ -75,13 +91,13 @@ app.showInfo = function(info)
 
 app.onStartButton = function()
 {
-	app.onStopButton();
+	app.onPauseButton();
 	app.startScan();
 	app.showInfo('Status: Scanning...');
 	app.startConnectTimer();
 };
 
-app.onStopButton = function()
+app.onPauseButton = function()
 {
 	// Stop any ongoing scan and close devices.
 	app.stopConnectTimer();
@@ -89,6 +105,33 @@ app.onStopButton = function()
 	evothings.easyble.closeConnectedDevices();
 	app.showInfo('Status: Stopped.');
 };
+
+app.onTareButton = function()
+{
+// zeros all acceleration values
+	app.onStartButton(); // collect one set of datapoints
+	app.tare['x'] = app.allData[0]['x'];
+	app.tare['y'] = app.allData[0]['y'];
+	app.tare['z'] = app.allData[0]['z'];
+	app.onStopButton(); // reset
+}
+
+app.onStopButton = function()
+{
+	app.onPauseButton();
+	app.dataPoints = [];
+	// data in the plot
+	app.allData = [];
+	// all data since start of test
+	app.allDataTime = [];
+	// sampling time steps since start of test
+	app.last_sampled_time = 0;
+	// raw time fo last sampled datapoint
+	app.arms = 0; 
+	// root mean square acceleration
+	app.peak = 0;
+	app.risk_level=0;
+}
 
 app.startConnectTimer = function()
 {
@@ -231,18 +274,38 @@ app.startAccelerometerNotification = function(device)
 		app.sensortag.MOVEMENT_DATA,
 		function(data)
 		{
+			var CONV_TO_MS2 = 9.80665*4;
+
 			app.showInfo('Status: Data stream active - accelerometer');
 			var dataArray = new Uint8Array(data);
 			var values = app.getAccelerometerValues(dataArray);
 			var d = new Date();
 
-			//document.getElementById('period').innerHTML = d.getTime() - app.last_sampled_time;
-			//app.last_sampled_time = app.d.getTime();
+			values['x'] = values['x'] - app.tare['x'];
+			values['y'] = values['y'] - app.tare['y'];
+			values['z'] = values['z'] - app.tare['z'];
+			// subtract tare values
+
+			document.getElementById('zacc').innerHTML = "Z acc: " + (values['z']*CONV_TO_MS2).toPrecision(app.SIGFIGS);
+			// update z acc label (m/s^2)
+
+			app.updatePeak(values);
+			// update waveform peak (max acc in raw units bewteen all axes)
+
+			app.allDataTime.push(d.getTime() - app.last_sampled_time);
+			document.getElementById('period').innerHTML = "Sampling period (ms): " + (d.getTime() - app.last_sampled_time); // update sampling period in ms(need to increase this)
+			app.last_sampled_time = d.getTime();
+			// update last sampled time
 
 			app.allData.push(values);
 			//add to all data array
 
-			app.rmsAccel(values);
+			// calculate root means square acceleration
+			document.getElementById('arms').innerHTML = app.rmsAccel().toPrecision(app.SIGFIGS);
+			document.getElementById('vdv').innerHTML = app.vdv().toPrecision(app.SIGFIGS); // calculate vdv
+			document.getElementById('cf').innerHTML = (app.peak*CONV_TO_MS2/document.getElementById('arms').innerHTML).toPrecision(app.SIGFIGS); // crest factor
+			//update metrics
+			document.getElementById('risk').innerHTML = app.riskLevel(document.getElementById('arms').innerHTML, document.getElementById('vdv').innerHTML, document.getElementById('cf').innerHTML).toPrecision(app.SIGFIGS);
 			app.drawDiagram(values);
 		},
 		function(errorCode)
@@ -251,27 +314,107 @@ app.startAccelerometerNotification = function(device)
 		});
 };
 
-app.rmsAccel = function(values)
-// arms = sqrt(1/T * integral_0_to_T (a_z^2*dt))
+/**app.rmsAccel = function(values)
+// average accel
 {
-	var CONV_TO_MS2 = 36.52 // this needs to be properly determined
+	var CONV_TO_MS2 = 9.80665*4 // this needs to be properly determined
+	//var CONV_TO_MS2 = 1;
 	var cum_arms = 0;
 	for (var i = 0; i < app.allData.length; i++){
-		cum_arms = cum_arms + app.allData[i]['z']; // just takes z for now
+		cum_arms = cum_arms + (app.allData[i]['z']* CONV_TO_MS2); // just takes z for now
 	}
-
-	cum_arms = cum_arms * CONV_TO_MS2 / (app.allData.length);
+.
+	cum_arms = cum_arms  / (app.allData.length);
 
 	document.getElementById('arms').innerHTML = cum_arms;
+}**/
+
+app.riskLevel = function(arms, vdv, cf)
+// takes in root mean square acceleration, vibration dose value, and crest factor, then computes a weighted average
+{
+	arms_weight=1.5E3;
+	vdv_weight=2E-2;
+	cf_weight=5E-5;
+	return (arms*arms_weight+vdv*vdv_weight+cf*cf_weight)/3;
 }
 
-/*app.integrate = function(vector, timestep)
+app.updatePeak = function(values)
+// returns new peak in raw unit form
+// takes in acceleration values of current sample
+{
+	if (values['x']>app.peak){
+		app.peak= values['x'];
+	}
+	if(values['y']>app.peak){
+		app.peak = values['y'];
+	}
+	if(values['z']>app.peak){
+		app.peak = values['z'];
+	}
+}
+
+app.rmsAccel = function()
+// arms = sqrt(1/T * integral_0_to_T (a_z^2*dt))
+{	
+	var MS_TO_SECOND = 1/1000;
+	var CONV_TO_MS2 = 9.80665*4;
+	var a_x2 = [];
+	var a_y2 = [];
+	var a_z2 = [];
+
+	var T = app.allDataTime.reduce(getSum)*MS_TO_SECOND;
+	// get total time length in seconds
+
+	for (var i = 0; i < app.allData.length; i++)
+	{
+		a_x2.push(Math.pow((app.allData[i]['x']*CONV_TO_MS2),2));
+		a_y2.push(Math.pow((app.allData[i]['y']*CONV_TO_MS2),2));
+		a_z2.push(Math.pow((app.allData[i]['z']*CONV_TO_MS2),2));
+	}
+
+	var rms_accel = Math.sqrt((1/T)*(app.integrate(a_x2, app.allDataTime)+app.integrate(a_y2, app.allDataTime)+app.integrate(a_z2, app.allDataTime)));
+
+	function getSum(total, num) 
+	{
+    	return total + num;
+	}
+
+	return rms_accel;
+}	
+
+app.vdv = function()
+{
+	var MS_TO_SECOND;
+	var CONV_TO_MS2 = 9.80665*4;
+	var a_x4 = [];
+	var a_y4 = [];
+	var a_z4 = [];
+
+	for (var i = 0; i < app.allData.length; i++)
+	{
+		a_x4.push(Math.pow((app.allData[i]['x']*CONV_TO_MS2),4));
+		a_y4.push(Math.pow((app.allData[i]['y']*CONV_TO_MS2),4));
+		a_z4.push(Math.pow((app.allData[i]['z']*CONV_TO_MS2),4));
+	}
+
+	var vdv = Math.pow(app.integrate(a_x4, app.allDataTime)+app.integrate(a_y4, app.allDataTime)+app.integrate(a_z4, app.allDataTime),1/4);
+	return vdv;
+}
+
+app.integrate = function(integrand, time)
+	//uses trapezoid rule to compute integral wrt time vector
 {
 	var integral = 0;
-	for (var i = 0; i < sampled_acc.length - 1; i++){
-		integral = integral + sampled_acc[i+1];
+	var timestep = 0;
+	var MS_TO_SECOND = 1/1000
+
+	for (var i = 0; i < integrand.length-1; i++){
+		timestep = time[i+1]*MS_TO_SECOND;
+		integral = integral + timestep*(integrand[i]+integrand[i+1])/2;
 	}
-}*/
+
+	return integral;
+}
 
 /**
  * Calculate accelerometer values from raw data for SensorTag 2.
@@ -284,9 +427,9 @@ app.getAccelerometerValues = function(data)
 	var divisors = { x: -16384.0, y: 16384.0, z: -16384.0 };
 
 	// Calculate accelerometer values.
-	var ax = evothings.util.littleEndianToInt16(data, 6) / divisors.x;
-	var ay = evothings.util.littleEndianToInt16(data, 8) / divisors.y;
-	var az = evothings.util.littleEndianToInt16(data, 10) / divisors.z;
+	var ax = evothings.util.littleEndianToInt16(data, 6)/ divisors.x;
+	var ay = evothings.util.littleEndianToInt16(data, 8)/ divisors.y;
+	var az = evothings.util.littleEndianToInt16(data, 10)/ divisors.z; // subtract gravity
 
 	// Return result.
 	return { x: ax, y: ay, z: az };
